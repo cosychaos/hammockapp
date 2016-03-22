13f3d1a67bd219fc6a68fb8d498a3f6b936392b4
Ina Tsetsova <inatsetsova@gmail.com>
2016-03-22 18:29:44 +0000

    infinite loop persists

--- app/js/app.js
+++ app/js/app.js
@@ -9,19 +9,19 @@
 			'ngRoute',
 			'ng-token-auth'
 			])
-	.config(function($routeProvider){
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 		$routeProvider
 			.when('/', {
 				templateUrl: 'index.html',
 				controller: 'IndexCtrl'
 			})
 			.when('/sign_in', {
-        templateUrl: '../../views/user_sessions/new.html',
+        templateUrl: '../views/user_sessions/new.html',
         controller: 'UserSessionsCtrl'
       })
-      ;
-		  // .otherwise({
-    //     redirectTo: '/'
-    //   });
-	});
+		  .otherwise({
+        redirectTo: '/'
+      });
+    // $locationProvider.html5Mode(true);
+	}]);
 }());
