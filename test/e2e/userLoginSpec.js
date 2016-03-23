865c0867f0b7089e64c9246a618670a69f7dc1df
Ina Tsetsova <inatsetsova@gmail.com>
2016-03-23 18:35:12 +0000

    tests are now passing

--- test/e2e/userLoginSpec.js
+++ test/e2e/userLoginSpec.js
@@ -28,13 +28,14 @@ describe('Hammock homepage', function() {
 
 		it('returns an error if wrong credentials', function(){
 			$('#sign_in').click();
-			$('#email').sendKeys("dog@dog.com");
+			$('#email').sendKeys("email@email.com");
 			$('#password').sendKeys("1234");
 			$('#submit-login').submit();
 			browser.ignoreSynchronization = true;
 		  browser.waitForAngular();
 		  browser.sleep(5000);
 		  expect($('.alert').getText()).toBe("Invalid login credentials. Please try again.");
+		  expect(browser.getCurrentUrl()).toContain('sign_in');
 			expect($('#sign_out').isDisplayed()).toBe(false);
 		});
 	});
