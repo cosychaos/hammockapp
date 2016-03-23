bd1a1186b3aea0700d65ed8203e34b3359eb53b1
Ina Tsetsova <inatsetsova@gmail.com>
2016-03-23 11:53:25 +0000

    error if wrong login credentials passes

--- test/e2e/userLoginSpec.js
+++ test/e2e/userLoginSpec.js
@@ -24,5 +24,17 @@ describe('Hammock homepage', function() {
 		  browser.sleep(5000);
 			expect($('#sign_out').isDisplayed()).toBe(true);
 		});
+
+		it('returns an error if wrong credentials', function(){
+			$('#sign_in').click();
+			$('#email').sendKeys("dog@dog.com");
+			$('#password').sendKeys("1234");
+			$('#submit-login').submit();
+			browser.ignoreSynchronization = true;
+		  browser.waitForAngular();
+		  browser.sleep(5000);
+		  expect($('.alert').getText()).toBe("Invalid login credentials. Please try again.");
+			expect($('#sign_out').isDisplayed()).toBe(false);
+		});
 	});
 });
