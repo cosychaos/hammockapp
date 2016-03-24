6400c7cd0e7607ee8b3db6ae37261f5df5056796
Ina Tsetsova <inatsetsova@gmail.com>
2016-03-24 17:21:16 +0000

    user classes displayed

--- test/e2e/userCoursesSpec.js
+++ test/e2e/userCoursesSpec.js
@@ -10,6 +10,14 @@ describe('User course page', function() {
 	describe("User courses", function() {
 		it ("says my courses", function(){
 			expect($('h2').getText()).toBe("My Courses");
+			var courses = element.all(by.css('.course'));
+			var firstCourse = courses.first();
+
+			var firstCourseName = firstCourse.element(by.css('.course-name'));
+			expect(firstCourseName.getText()).toBe('The joy of physics');
+
+			var firstCourseProvider = firstCourse.element(by.css('.course-provider'));
+			expect(firstCourseProvider.getText()).toBe('Coursera');
 		});
 	});
 });
